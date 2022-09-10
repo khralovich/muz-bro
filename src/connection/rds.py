1c415f8f70f9f9425c49b60094522888a0482d7a
Pavel Nikalayeu <pavelnikalayeu@pavels-mbp-2.speedport.ip>
2022-09-10 17:40:15 +0200

    Implement reading songs from DB

--- src/connection/rds.py
+++ src/connection/rds.py
@@ -3,6 +3,8 @@ from sqlalchemy import create_engine
 from config import RDS_ENDPOINT, RDS_USERNAME, RDS_PASSWORD, RDS_PORT, RDS_DB_NAME
 
 sql_alchemy_db_url = f"postgresql://{RDS_USERNAME}:{RDS_PASSWORD}@{RDS_ENDPOINT}/{RDS_DB_NAME}"
+
+
 def create_connection():
     return psycopg2.connect(
         database=RDS_DB_NAME,
@@ -15,3 +17,25 @@ def create_connection():
 def create_db_engine():
     engine = create_engine(sql_alchemy_db_url)
     return engine
+
+def read_songs(genre: str = None, mood: str = None):
+    if genre is None and mood is None:
+        # None of genre/mood selected
+        query = "select * from public.songs"
+    elif genre is not None:
+        if mood is None:
+            # Only genre selected
+            query = f"select * from public.songs where '{genre}' = ANY(genre)"
+        else:
+            # Both genre and mood selected
+            query = f"select * from public.songs where {genre}' = ANY(genre) and '{mood}' = ANY(mood)"
+    else:
+        # Only mood selected
+        query = f"select * from public.songs where '{mood}' = ANY(mood)"
+    
+    with create_connection() as connection:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        connection.commit()
+    return result
