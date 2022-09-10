5329f10a043a8f3d7be647cbee26f640acead623
khralovich <khralovich@protonmail.com>
2022-09-10 13:43:01 +0200

    change btns to inline w callbacks

--- bot/main.py
+++ bot/main.py
@@ -2,35 +2,63 @@ from telegram import *
 from telegram.ext import *
 from config import API_KEY
 from btns import *
+from vars import *
 
 # https://docs.python-telegram-bot.org/en/stable/telegram.ext.filters.html#telegram.ext.filters.Filters
 
 updater = Updater(API_KEY, use_context=True)
-dispatcher = updater.dispatcher
-
-def startCommand(update: Update, context: CallbackContext):
-    buttons = [[KeyboardButton("Здзіві мяне!"),
-                KeyboardButton("Рэп"),
-                KeyboardButton("Фолк")],
-               [KeyboardButton("Поп"),
-                KeyboardButton("Альт рок"),
-                KeyboardButton("Метал")],
-               [KeyboardButton("Індзі рок"),
-                KeyboardButton("Электроніка"),
-                KeyboardButton("->")]]
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Прывітанне! Я твой музычны бро :) Што паслухаем? Калі не ведаеш, з чаго пачаць — абяры /help",
-                             reply_markup=ReplyKeyboardMarkup(buttons))
 
 
-def messageHandler(update: Update, context: CallbackContext):
-    if "Здзіві мяне!" in update.message.text:
-        text = keysOutput["Здзіві мяне!"]
-    if "Рэп" in update.message.text:
-        text = keysOutput["Рэп"]
-    if "Электроніка" in update.message.text:
-        text = keysOutput["Электроніка"]
-    if text:
-        context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+def start(update: Update, context: CallbackContext):
+  context.bot.send_message(chat_id=update.effective_chat.id, text="Прывітанне! Я твой музычны бро :) Што паслухаем? Калі не ведаеш, з чаго пачаць — абяры /help",
+                         reply_markup=main_menu_keyboard())
+
+def main_menu(bot, update):
+  bot.callback_query.message.edit_text(main_menu_message(),
+                          reply_markup=main_menu_keyboard())
+
+def first_menu(bot, update):
+  bot.callback_query.message.edit_text(first_menu_message(),
+                          reply_markup=first_menu_keyboard())
+
+def second_menu(bot, update):
+  bot.callback_query.message.edit_text(second_menu_message(),
+                          reply_markup=second_menu_keyboard())
+
+def first_submenu(bot, update):
+  pass
+
+def second_submenu(bot, update):
+  pass
+
+def error(update, context):
+    print(f'Update {update} caused error {context.error}')
+
+############################ Keyboards #########################################
+def main_menu_keyboard():
+  keyboard = [[InlineKeyboardButton('Здзіві мяне!', callback_data='m1')],
+              [InlineKeyboardButton('Фолк', callback_data='m2')],
+              [InlineKeyboardButton('Поп', callback_data='m3')],
+              [InlineKeyboardButton('Метал', callback_data='m3')],
+              [InlineKeyboardButton('Індзі рок', callback_data='m3')],
+              [InlineKeyboardButton('Рэп', callback_data='m3')],
+              [InlineKeyboardButton('Электроніка', callback_data='m3')]
+              ]
+  return InlineKeyboardMarkup(keyboard)
+
+def first_menu_keyboard():
+  keyboard = [[InlineKeyboardButton('Submenu 1-1', callback_data='m1_1')],
+              [InlineKeyboardButton('Submenu 1-2', callback_data='m1_2')],
+              [InlineKeyboardButton('Назад', callback_data='main')]]
+  return InlineKeyboardMarkup(keyboard)
+
+def second_menu_keyboard():
+  keyboard = [[InlineKeyboardButton('Submenu 2-1', callback_data='m2_1')],
+              [InlineKeyboardButton('Submenu 2-2', callback_data='m2_2')],
+              [InlineKeyboardButton('Назад', callback_data='main')]]
+  return InlineKeyboardMarkup(keyboard)
+
+############################# Helpers #########################################
 
 
 def help(update: Update, context: CallbackContext):
@@ -39,13 +67,33 @@ def help(update: Update, context: CallbackContext):
     """)
 
 def info(update: Update, context: CallbackContext):
-    update.message.reply_text("Мы зрабілі гэты бот, каб ты мог/магла атрымліваць асалоду ад крутой беларускай музыкі ў любой кропцы свету. Распавядзі нам пра свой настрой і свае ўлюбёныя жанры і пачнем! Скарыстайся з кнопак, каб абраць жанр:")
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Мы зрабілі гэты бот, каб ты мог/магла атрымліваць асалоду ад крутой беларускай музыкі ў любой кропцы свету. Распавядзі нам пра свой настрой і свае ўлюбёныя жанры і пачнем! Скарыстайся з кнопак, каб абраць жанр:",
+                             reply_markup=main_menu_keyboard())
 
 
-dispatcher.add_handler(CommandHandler("start", startCommand))
-dispatcher.add_handler(CommandHandler('help', help))
-dispatcher.add_handler(CommandHandler('info', info))
-dispatcher.add_handler(MessageHandler(Filters.text, messageHandler))
 
+
+
+############################# Messages #########################################
+def main_menu_message():
+  return 'Choose the option in main menu:'
+
+def first_menu_message():
+  return 'Choose the submenu in first menu:'
+
+def second_menu_message():
+  return 'Choose the submenu in second menu:'
+
+############################# Handlers #########################################
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
+updater.dispatcher.add_handler(CallbackQueryHandler(first_menu, pattern='m1'))
+updater.dispatcher.add_handler(CallbackQueryHandler(second_menu, pattern='m2'))
+updater.dispatcher.add_handler(CallbackQueryHandler(first_submenu, pattern='m1_1'))
+updater.dispatcher.add_handler(CallbackQueryHandler(second_submenu, pattern='m2_1'))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(CommandHandler('info', info))
+updater.dispatcher.add_error_handler(error)
 
 updater.start_polling()
