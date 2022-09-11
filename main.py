c92702d4597513422d0c8c6ec6ffc332a3d3f9ee
khralovich <khralovich@protonmail.com>
2022-09-11 12:22:16 +0200

    fix back btn

--- main.py
+++ main.py
@@ -3,25 +3,21 @@ from telegram import *
 from telegram.ext import Updater, CallbackContext, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
 from config import API_KEY
 
-# https://docs.python-telegram-bot.org/en/stable/telegram.ext.filters.html#telegram.ext.filters.Filters
-
 updater = Updater(API_KEY, use_context=True)
 
 genre = None
 mood = None
 
-
 # HTML szablon
 
 all_genres = ['Здзіві мяне!', "Фолк", 'Поп', 'Метал', 'Індзі', 'Рэп', 'Рок', 'Электроніка', 'Skip genre']
 all_moods = ['Імпрэза', 'Спорт', 'Рамантыка', 'Разбітае сэрцайка', 'Медытацыя','Чыл','У дарозе','Самота','Надзея','Праца\вучоба','Skip mood']
+goback = ["Назад"]
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text="Прывітанне! Я твой музычны бро :) Што паслухаем? Калі не ведаеш, з чаго пачаць — абяры /help",
                              reply_markup=main_menu_keyboard())
-
-
 
 def main_menu(update: Update, context: CallbackContext):
     reply = update.callback_query.data
@@ -31,9 +27,12 @@ def main_menu(update: Update, context: CallbackContext):
         print(genre)
     if reply in all_moods:
         mood = reply
-        update.callback_query.answer()
         print(mood)
         context.bot.send_message(chat_id=update.effective_chat.id, text="<u>Тут будзе ХТМЛ</u>", parse_mode="HTML" ,reply_markup=like_buttons())
+    if reply in goback:
+        print(reply)
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Давай пачнем з выбару жанру. Абяры які-небудзь з наступных:",
+                                 reply_markup=main_menu_keyboard())
 
 
 
@@ -58,11 +57,9 @@ def main_menu_keyboard():
 
 def like_buttons():
     keyboard = [[InlineKeyboardButton("Хачу яшчэ", callback_data="more")],
-                [InlineKeyboardButton("Назад да жанраў", callback_data="back")]
+                [InlineKeyboardButton("Назад да жанраў", callback_data="Назад")]
                 ]
-
     return InlineKeyboardMarkup(keyboard)
-
 
 
 def first_menu_keyboard():
@@ -76,8 +73,8 @@ def first_menu_keyboard():
                 [InlineKeyboardButton('Самота', callback_data='Самота')],
                 [InlineKeyboardButton('Надзея', callback_data='Надзея')],
                 [InlineKeyboardButton('Праца\вучоба', callback_data='Праца\вучоба')],
-                [InlineKeyboardButton('Прапусціць', callback_data='Skip')],
-                [InlineKeyboardButton('Назад', callback_data="Skip mood")]
+                [InlineKeyboardButton('Прапусціць', callback_data='Skip mood')],
+                [InlineKeyboardButton('Назад', callback_data="Назад")]
     ]
     return InlineKeyboardMarkup(keyboard)
 
@@ -100,7 +97,6 @@ def info(update: Update, context: CallbackContext):
 ############################# Messages #########################################
 def main_menu_message():
     return 'Абяры стыль музыкі, якую хочаш паслухаць:'
-
 
 def first_menu_message():
     return 'Абяры настрой, які адпавядае сітуацыі, а мы паклапоцімся пра музыку:'
