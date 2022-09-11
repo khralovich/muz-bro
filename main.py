360d9da265155049a82633effef40df82fe0376c
Pavel Nikalayeu <pavelnikalayeu@pavels-mbp-2.speedport.ip>
2022-09-11 15:09:46 +0200

    remove prints

--- main.py
+++ main.py
@@ -44,7 +44,6 @@ def main_menu(update: Update, context: CallbackContext):
         songs = read_songs(genre="test", mood=None, shown_songs=shown_songs)
         n_songs = len(songs)
         print(n_songs, " Songs")
-        print(HTML_TEMPLATE)
         if n_songs >= 5:
             html_render = HTML_TEMPLATE.render(
             artist_1=songs[0]['artist'], title_1=songs[0]['title'],
@@ -58,7 +57,6 @@ def main_menu(update: Update, context: CallbackContext):
             platforms_4=[{"name": name, "url": url} for name, url in songs[3].items() if name in SELECTED_PLATFORMS],
             platforms_5=[{"name": name, "url": url} for name, url in songs[4].items() if name in SELECTED_PLATFORMS],
             )
-            print(html_render)
             context.bot.send_message(chat_id=update.effective_chat.id, text=html_render, parse_mode="HTML" ,reply_markup=like_buttons())
             for song in songs[:5]:
                 if song['artist'] not in shown_songs:
